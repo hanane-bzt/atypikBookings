77c57fa313b353f36a13f1cc6447e1976dc3126f
hanane-bzt <hananallalou@gmail.com>
2025-09-08 05:44:13 +0200

    add modif vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,8 +18,7 @@
 //   },
 // }));
 
-import { defineConfig } from 'vitest';
-import { loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
